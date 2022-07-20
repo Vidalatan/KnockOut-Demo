8529d5072c397aeaa9c1c9881394d2aa07485370
Vidalatan <vidalatan@gmail.com>
2022-07-20 10:21:58 -0500

    Popstate page load working

--- koscript.js
+++ koscript.js
@@ -1,15 +1,24 @@
 function testViewModel(){
   const self = this;
 
+  self.navLinks = ['Home', 'About', 'Contact', 'Resume']
+
+  self.currentWindow = ko.observable('#!')
   self.counter = ko.observable(0)
-  
-  self.increase = function(){
-    this.counter(this.counter()+1)
-  }
-  self.decrease = function(){
-    this.counter(this.counter()-1)
+  self.navClicked = ko.observable(false)
+
+  self.navClick = function(e) {
+    const l = e === 'Home' ? `#!` : `#!${e.toLowerCase()}`;
+    (self.navClicked() || self.navClicked(true));
+    window.history.pushState({}, '', `index.html${l}`);
+    self.currentWindow(l);
   }
 
+  self.watchWindow = function(){
+    window.addEventListener('popstate', (e) => self.currentWindow(e.target.location.href.slice(e.target.location.href.indexOf('#!'))))
+  }
+
+  self.watchWindow()
 }
 
 ko.applyBindings(new testViewModel());
