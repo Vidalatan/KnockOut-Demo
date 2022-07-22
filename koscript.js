18d9b1691ff573f7a9bb4222ab850bd4de98fc37
Vidalatan <vidalatan@gmail.com>
2022-07-22 02:30:52 -0500

    Added dialogue
    Final dialogue condition dependent on forward/backward button

--- koscript.js
+++ koscript.js
@@ -5,17 +5,28 @@ function testViewModel(){
 
   self.currentWindow = ko.observable('#!')
   self.counter = ko.observable(0)
-  self.navClicked = ko.observable(false)
+  self.navClicked = ko.observable(false);
+  self.restrictHome = ko.observable(true);
+  self.lstMsg = ko.observable(false)
+  self.progMsg = ko.observable('Thought you would see the same message still? Nope. Also, have you tried using the back or forward buttons on your browser yet? Give it a try!')
 
   self.navClick = function(e) {
     const l = e === 'Home' ? `#!` : `#!${e.toLowerCase()}`;
-    (self.navClicked() || self.navClicked(true));
+    (l === '#!' || self.navClicked() || self.navClicked(true));
     window.history.pushState({}, '', `index.html${l}`);
     self.currentWindow(l);
   }
 
   self.watchWindow = function(){
-    window.addEventListener('popstate', (e) => self.currentWindow(e.target.location.href.slice(e.target.location.href.indexOf('#!'))))
+    window.addEventListener('popstate', (e) => {
+      if(self.navClicked() === true) self.lstMsg() ? self.progMsg(`As you can see, the back and forward buttons work normally as 
+      you\'d expect. This is a result of listening to the popstate event from the window and pushing certain url locations to the 
+      history, while also setting conditional rendering depending on the url. This makes it look like we are changing pages, and allows 
+      for the url to be manipulated appropriately by the forward and back buttons, but in fact this is still just a single page website. 
+      
+      Stay tuned, there will be more content to come!`) : self.lstMsg(true);
+      self.currentWindow(e.target.location.href.slice(e.target.location.href.indexOf('#!')))
+    })
   }
 
   self.watchWindow()
